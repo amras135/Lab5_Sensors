67d0582145b7d4d98751a8c3377760a5a54901c9
Amer Amr <amras@vcu.edu>
2017-11-10 14:54:18 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -1,128 +1,39 @@
 #include "stm32l476xx.h"
-#include "lcd.h"
+#include "SysClock.h"
+#include "ADC.h"
 
-//*************************************  32L476GDISCOVERY ***************************************************************************
-// STM32L4:  STM32L476VGT6 MCU = ARM Cortex-M4 + FPU + DSP, 
-//           LQFP100, 1 MB of Flash, 128 KB of SRAM
-//           Instruction cache = 32 lines of 4x64 bits (1KB)
-//           Data cache = 8 lines of 4x64 bits (256 B)
-//
-// Joystick (MT-008A): 
-//   Right = PA2        Up   = PA3         Center = PA0
-//   Left  = PA1        Down = PA5
-//
-// User LEDs: 
-//   LD4 Red   = PB2    LD5 Green = PE8
-//   
-// CS43L22 Audio DAC Stereo (I2C address 0x94):  
-//   SAI1_MCK = PE2     SAI1_SD  = PE6    I2C1_SDA = PB7    Audio_RST = PE3    
-//   SAI1_SCK = PE5     SAI1_FS  = PE4    I2C1_SCL = PB6                                           
-//
-// MP34DT01 Digital MEMS microphone 
-//    Audio_CLK = PE9   Audio_DIN = PE7
-//
-// LSM303C eCompass (a 3D accelerometer and 3D magnetometer module): 
-//   MEMS_SCK  = PD1    MAG_DRDY = PC2    XL_CS  = PE0             
-//   MEMS_MOSI = PD4    MAG_CS  = PC0     XL_INT = PE1       
-//                      MAG_INT = PC1 
-//
-// L3GD20 Gyro (three-axis digital output): 
-//   MEMS_SCK  = PD1    GYRO_CS   = PD7
-//   MEMS_MOSI = PD4    GYRO_INT1 = PD2
-//   MEMS_MISO = PD3    GYRO_INT2 = PB8
-//
-// ST-Link V2 (Virtual com port, Mass Storage, Debug port): 
-//   USART_TX = PD5     SWCLK = PA14      MFX_USART3_TX   MCO
-//   USART_RX = PD6     SWDIO = PA13      MFX_USART3_RX   NRST
-//   PB3 = 3V3_REG_ON   SWO = PB5      
-//
-// Quad SPI Flash Memory (128 Mbit)
-//   QSPI_CS  = PE11    QSPI_D0 = PE12    QSPI_D2 = PE14
-//   QSPI_CLK = PE10    QSPI_D1 = PE13    QSPI_D3 = PE15
-//
-// LCD (24 segments, 4 commons, multiplexed 1/4 duty, 1/3 bias) on DIP28 connector
-//   VLCD = PC3
-//   COM0 = PA8     COM1  = PA9      COM2  = PA10    COM3  = PB9
-//   SEG0 = PA7     SEG6  = PD11     SEG12 = PB5     SEG18 = PD8
-//   SEG1 = PC5     SEG7  = PD13     SEG13 = PC8     SEG19 = PB14
-//   SEG2 = PB1     SEG8  = PD15     SEG14 = PC6     SEG20 = PB12
-//   SEG3 = PB13    SEG9  = PC7      SEG15 = PD14    SEG21 = PB0
-//   SEG4 = PB15    SEG10 = PA15     SEG16 = PD12    SEG22 = PC4
-//   SEG5 = PD9     SEG11 = PB4      SEG17 = PD10    SEG23 = PA6
-// 
-// USB OTG
-//   OTG_FS_PowerSwitchOn = PC9    OTG_FS_VBUS = PC11    OTG_FS_DM = PA11  
-//   OTG_FS_OverCurrent   = PC10   OTG_FS_ID   = PC12    OTG_FS_DP = PA12  
-//
-// PC14 = OSC32_IN      PC15 = OSC32_OUT
-// PH0  = OSC_IN        PH1  = OSC_OUT 
-// 
-// PA4  = DAC1_OUT1 (NLMFX0 WAKEUP)   PA5 = DAC1_OUT2 (Joy Down)
-// PA3  = OPAMP1_VOUT (Joy Up)        PB0 = OPAMP2_VOUT (LCD SEG21)
-//
-//****************************************************************************************************************
+volatile uint32_t digoutput;
+//volatile uint32_t x;
+//volatile uint32_t y;
+volatile double voltage;
+volatile double distance;
 
-volatile uint8_t hour,min,sec; 
-
-/*----------------------------------------------------------------------------
-  SysTick_Handler
- *----------------------------------------------------------------------------*/
-void SysTick_Handler(void) {
-  uint8_t dig[8];
-
-  sec++;
-	if (sec==60) {
-		sec=0;
-		min++;
-		if (min==60) {
-			min=0;
-			hour++;
-			if (hour==12) 
-				hour=0;
-		}
-	}
-	
-	
-		dig[0]=hour/10 +'0';
-		dig[1]=hour%10 +'0';
-		dig[2]=':';
-		dig[3]=min/10 +'0';
-		dig[4]=min%10 +'0';
-		dig[5]=':';
-		dig[6]=sec/10 +'0';
-		dig[7]=sec%10 +'0';
-
-	
-	LCD_DisplayString(dig);
-}
-
-
-void System_Clock_Init(void);
-uint32_t SystemCoreClock    = 16000000;
+//volatile double result;
 
 int main(void){
-	hour= min =sec = 0;
-	System_Clock_Init();
-	 //if (SysTick_Config(SystemCoreClock)) { /* SysTick 1 sec interrupts  */
-    //while (1);                                  /* Capture error              */
-  //}
+	//double result = 0;
 	
-	LCD_Initialization();
-	LCD_Clear();
-	//LCD_DisplayString((uint8_t*)"HH:MM:SS");
-	while(1) {
-		LCD_DisplayName();
+	//double z = 0;
+	System_Clock_Init(); // Switch System Clock = 80 MHz
+	
+	// Analog Inputs: 
+	//  PA1 (ADC12_IN6), PA2 (ADC12_IN7)
+	//  These pins are not used: PA0 (ADC12_IN5, PA3 (ADC12_IN8) 
+	//  0V <=> 0, 3.3V <=> 4095
+	ADC_Init();
+	
+
+	
+	while(1){
+		ADC1->CR |= ADC_CR_ADSTART;			
+		while ( (ADC123_COMMON->CSR | ADC_CSR_EOC_MST) == 0);
+		digoutput = ADC1->DR;
+		
+		z = (double)(digoutput)/(double)(1241);
+
+		
 	}
-		//LCD_bar();
-	
 }
 
-void System_Clock_Init(void){
-	
-	// Enable the Internal High Speed oscillator (HSI)
-	RCC->CR |= RCC_CR_HSION;
-	while((RCC->CR & RCC_CR_HSIRDY) == 0);
-	// Adjusts the Internal High Speed oscillator (HSI) calibration value
-	// Select HSI is used as system clock
-}
+
 
